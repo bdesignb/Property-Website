39d0846b82ea37b2414d6c28341c54412151ed70
bdesignb <bozana.bundalo@gmail.com>
2020-03-22 08:06:03 +0100

    Updates

---
+++ precache-manifest.92c2ca3be16311b326f758d14fe099f9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bca25f0342901e946d6ccd7e01eeb5b",
+    "url": "/Property-Website/index.html"
+  },
+  {
+    "revision": "a9f24bb14892b741e0a3",
+    "url": "/Property-Website/static/css/main.0dda6374.chunk.css"
+  },
+  {
+    "revision": "34f5edde846b6b6ef5da",
+    "url": "/Property-Website/static/js/2.cd63470f.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/Property-Website/static/js/2.cd63470f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9f24bb14892b741e0a3",
+    "url": "/Property-Website/static/js/main.3ac8d4af.chunk.js"
+  },
+  {
+    "revision": "7ccaca445451d8eef9f5",
+    "url": "/Property-Website/static/js/runtime-main.dfa5280d.js"
+  },
+  {
+    "revision": "954a9b499800363f808b16b08f882015",
+    "url": "/Property-Website/static/media/header_hero.954a9b49.jpg"
+  },
+  {
+    "revision": "e161e4db3cd3ab2bfbc71cc35fbbdff1",
+    "url": "/Property-Website/static/media/logo.e161e4db.png"
+  },
+  {
+    "revision": "3ccd793d9c58a278e5ed0bc0c2fe6fce",
+    "url": "/Property-Website/static/media/property_hero.3ccd793d.jpg"
+  }
+]);
